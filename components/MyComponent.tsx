1f4b00ec35322a60c14d24bd930730f6b0cefcee
solserj <fgump259@gmail.com>
2025-01-07 15:50:20 +0200

    lesson1.3 basic and modular styles

--- components/MyComponent.tsx
+++ components/MyComponent.tsx
@@ -1,10 +1,23 @@
 import {FC} from 'react';
+import './MyComponent.css';
 
 type MyComponentPropType = {text: string};
 
 const  MyComponent : FC<MyComponentPropType> = ({text}) => {
-    return (<div>{text}</div>);
+    return (<div className={'target'}>{text}</div>);
 };
 
 export default MyComponent;
 
+
+// нижче показаний модульний варіант імпорту css
+// import {FC} from 'react';
+// import styles from './MyComponent.module.css';
+//
+// type MyComponentPropType = {text: string};
+//
+// const  MyComponent : FC<MyComponentPropType> = ({text}) => {
+//     return (<div className={styles.target}>{text}</div>);
+// };
+//
+// export default MyComponent;
