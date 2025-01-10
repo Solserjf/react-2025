4402197fa914ad2e5c227ca26745a11d32bccfcb
solserj <fgump259@gmail.com>
2025-01-10 18:52:03 +0200

    question for consultation 10/01/2025

--- src/components/todocomponents/TodoComponents.tsx
+++ src/components/todocomponents/TodoComponents.tsx
@@ -18,3 +18,9 @@ export const TodoComponents = () => {
         </>
     );
 };
+//components, setComponents, <IModel> - це не по контексту.
+// А ось завдання: Task 1 ohm4AivCEBv
+// https://jsonplaceholder.typicode.com/todos - тут масив даних. ознайомтесь з ним для початку
+// Створити компонент TodoComponent який буде містити інформацію про окремий об'єкт з масиву.
+// Створити компонент TodoComponents в якому відбувається ітерація масиву даних отриманих з апі.
+// Кореневим компонентом є App
