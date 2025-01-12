c89e801e6774b1999503799026bc4733615ff92b
solserj <fgump259@gmail.com>
2025-01-12 23:14:06 +0200

    end of lesson 3.1 basic router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,33 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(<App />)
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.tsx";
+import {APage} from "./pages/APage.tsx";
+import {BPage} from "./pages/BPage.tsx";
+import {HomePage} from "./pages/HomePage.tsx";
+const router = createBrowserRouter(
+    [
+        {path: '/', element:<MainLayout/>,
+            children: [
+                // /a
+                {path:'a', element: <APage/>},
+                {path:'b', element: <BPage/>, children: [
+                    // /b/1
+                        {path: '1', element: <div>content 1</div>},
+                        // /b/2
+                        {path: '2', element: <div>content 2</div>},
+                        // /b/3
+                        {path: '3', element: <div>content 3</div>},
+                    ]},
+
+                {path:'', element: <HomePage/>},
+            ]
+        },
+
+
+    ]
+);
+
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/> );
---
+++ src/pages/APage.tsx
@@ -0,0 +1,5 @@
+export const APage = () => {
+    return (
+        <div>APage</div>
+    );
+};
