b719e02d5ddf0d8acc2ca25a6b052cc20127f455
solserj <fgump259@gmail.com>
2025-01-13 17:18:57 +0200

    end of lesson 3.3 state forward

---
+++ src/pages/SingleUserDetailsPage.tsx
@@ -0,0 +1,12 @@
+import {useLocation} from "react-router-dom";
+import {IUser} from "../components/model/IUser.ts";
+
+export const SingleUserDetailsPage = () => {
+    const {state} = useLocation();
+    const user = state as IUser;
+    return (
+        <div>
+            {user.email}
+        </div>
+    );
+};
