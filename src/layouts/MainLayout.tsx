67d09ae2df9544dea5f275f3200f02d9f4f8dd73
solserj <fgump259@gmail.com>
2025-01-16 15:48:15 +0200

    homework task 3.3 Routing

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router";
+import {MenuComponent} from "../components/menu-component/MenuComponent.tsx";
 
 export const MainLayout = () => {
     return (
         <div>
+            <MenuComponent/>
             <Outlet/>
         </div>
     );
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,9 +5,8 @@ import {CartsPage} from "../pages/CartsPage.tsx";
 
 export const routes = createBrowserRouter([{
     path: '/', element: <MainLayout/>, children: [
-        {path: 'users', element: <UsersPage/>, children:[
-                {path:':id/baskets', element:<CartsPage/>}
-            ]},
+        {path: 'users', element: <UsersPage/>},
+        {path:'users/:id/baskets', element:<CartsPage/>}
     ]
 }]);
 
