89eb6f7444fdcbf53c84081503dba8182a283caa
solserj <fgump259@gmail.com>
2025-01-24 21:00:21 +0200

    end of lesson 7.2 useCallback

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,6 +1,6 @@
-import {memo} from "react";
+import {FC, memo} from "react";
 
-export const UserComponent = memo( () => {//ф-ція мемомізації, щоб UserComponent повторно не рендерився
+export const UserComponent:FC<{foo: () => void}> = memo( () => {//ф-ція мемомізації, щоб UserComponent повторно не рендерився
     console.log('User');   // компонент UserComponent не буде рендеритися якщо він (UserComponent) не буде змінюватися(тобто йому не будуть приходити всередину ніякі props)
 
     return (
--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,19 +1,25 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export const UsersComponent = () => {
     console.log('Users');
     const [users, setUsers] = useState([]);
+    const foo = useCallback(() => {//ця ф-ція перестворюється щоразу як відбувається рірендер
+        console.log('test');// якщо будуть аргументи у ф-ції то їх потрібно буде визначити у залежностях(пустий масив над useEffect)
+    }, []) // таким чином ф-ція відпрацює тільки раз бо ми  її закешували
     useEffect(() => {
          fetch('https://jsonplaceholder.typicode.com/users').then(value => value.json()).then(value => {
-             setUsers(value);// якщо ми щось сетаємо(setUsers(value)) - відбувається updating компоненту що викликає ререндер цього компоненту
+             setUsers(value);// якщо ми щось сетаємо(setUsers(value)) - відбувається updating компоненту що викликає рірендер цього компоненту
              })//мемоізувати потрібно 'важкі' компоненти, де є багато логіки, цикли, бо мемоізація несе сповільнення, і прості звичні компоненти немає сенсу мемоізовувати щоб не сповільнювати с-му
             console.log(users);
+         return () => {
+             console.log('unsubscribe');
+         }
     }, []);
     return (
         <div>
             users component
-            <UserComponent/>
+            <UserComponent foo={foo}/> // тобто ми кешуємо тепер не тільки компонент а і його props як в даному випадку з ф-цію foo яка тут виступає як props
         </div>
     );
 };
