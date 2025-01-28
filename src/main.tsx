c05155b006226ebef362f2b9bd0ae6804bef51c8
solserj <fgump259@gmail.com>
2025-01-28 16:31:22 +0200

    end of lesson 8 redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
-
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {RouterProvider} from "react-router-dom";
+import {router} from "./router/router.tsx";
+import {Provider} from "react-redux";
+import {store} from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(<App />)
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <RouterProvider router={router}/>
+
+    </Provider>
+);
---
+++ src/pages/UsersPage.tsx
@@ -0,0 +1,27 @@
+import {useEffect} from "react";
+import {useAppSelector} from "../redux/hooks/useAppSelector.tsx";
+import {userSliceActions} from "../redux/slices UserSlice/userSlice.ts";
+import {useAppDispatch} from "../redux/hooks/useAppDispatch.tsx";
+
+export const UsersPage = () => {
+    const {users,loadState} = useAppSelector(({userSlice}) => userSlice);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(userSliceActions.loadUsers());
+    }, []);
+
+    return (
+        <div>
+            {!loadState && <div>Loading</div>}
+
+            {
+                users.map((user) => {
+                    return <div key={user.id}>{user.name}</div>
+                })
+            }
+
+
+        </div>
+    );
+};
